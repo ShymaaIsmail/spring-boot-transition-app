9020bf0f2e550314b0c92500d29b325a130a44a3
shymaamismail <s.ismail@takamol.com.sa>
2021-09-02 16:28:28 +0300

    annotations is progress

---
+++ src/main/java/com/example/transitiondemoapp/transitiondemo/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.example.transitiondemoapp.transitiondemo.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository   
+public class EmployeeRepository  
+{  
+public void add()  
+{     
+//persistence code  
+}  
+
+}  
---
+++ src/main/java/com/example/transitiondemoapp/transitiondemo/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.example.transitiondemoapp.transitiondemo.service;
+import org.springframework.stereotype.Service;
+
+@Service  
+public class EmployeeService  
+{  
+public void hireEmployee()  
+{  
+//business code  
+}  
+}  
